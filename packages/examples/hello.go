fee1f43bd1b89f02bda17fdf588244f66acc9eb2
~venir <luca@venir.dev>
2024-07-14 16:47:02 +0100

    gave a proper name

--- packages/examples/hello.go
+++ packages/examples/hello.go
@@ -22,7 +22,7 @@ package main
 import "fmt"
 
 // Main function for the action
-func Main(obj map[string]interface{}) map[string]interface{} {
+func HelloWorld(obj map[string]interface{}) map[string]interface{} {
 	name, ok := obj["name"].(string)
 	if !ok {
 		name = "world"
